2667c988d996a4e3c8982e8a2a079e992f427564
Philippe Laurendeau <phytoporg@github.com>
2024-02-26 15:49:40 -0800

    ScenariosPlugin initial scaffolding

---
+++ src/ScenariosPlugin/ScenariosPlugin.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace ScenariosPlugin;
+public class ScenariosPlugin : BasePlugin
+{
+    public override string ModuleName => "Scenarios Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        // TODO: something interesting
+    }
+}
