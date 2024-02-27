8a6f0f9e6c0c04302b286e40d9f4d52cc2af79d9
Philippe Laurendeau <phytoporg@github.com>
2024-02-26 17:34:24 -0800

    Stopping point: working on loading scenarios

---
+++ src/ScenariosPlugin/ScenarioConfig.cs
@@ -0,0 +1,60 @@
+﻿using CounterStrikeSharp.API.Modules.Utils;
+using System.Collections.Generic;
+
+namespace ScenariosPlugin
+{
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    public struct PlayerPlacement
+    {
+        // The 3D position of a player on the target map
+        public Vector3 Position;
+
+        // The player's angle/look vector
+        public Vector3 Angle;
+
+        // Team assignment for the player
+        public CsTeam Team;
+
+        // Must this placement be filled by a player for a scenario, or can it be empty?
+        public bool IsRequired;
+
+        // TODO: initial weapons state
+        // TODO: initial utility state
+        // TODO: win condition(s)
+    }
+
+    public enum Map
+    {
+        Invalid,
+        Mirage
+    }
+
+    public class ScenarioConfig
+    {
+        // The name of this configuration
+        public string Name { get; set; }
+
+        // The map for this scenario
+        public Map Map { get; set; }
+
+        // Time limit for the scenario, or -1 if there is no limit
+        public int TimeLimitSeconds { get; set; }
+
+        // A list of initial player placements
+        public List<PlayerPlacement> PlayerPlacements { get; set; }
+
+        public ScenarioConfig()
+        {
+            Name = "Unnamed";
+            Map = Map.Invalid;
+            PlayerPlacements = new List<PlayerPlacement>();
+            TimeLimitSeconds = -1;
+        }
+    }
+}
--- src/ScenariosPlugin/ScenariosPlugin.cs
+++ src/ScenariosPlugin/ScenariosPlugin.cs
@@ -1,14 +1,73 @@
-﻿using CounterStrikeSharp.API.Core;
-
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Logging;
 namespace ScenariosPlugin;
+
 public class ScenariosPlugin : BasePlugin
 {
     public override string ModuleName => "Scenarios Plugin";
-
     public override string ModuleVersion => "0.0.1";
+
+    private List<ScenarioConfig> _scenarioConfigs = new List<ScenarioConfig>();
+
+    [ConsoleCommand("load_scenario", "Loads a scenario. Must be on the correct map, first.")]
+    [CommandHelper(minArgs: 1, usage: "[scenario]", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    public void LoadScenarioCommand(CCSPlayerController? player, CommandInfo command)
+    {
+        if (player == null)
+        {
+            Logger.LogWarning("Player was null in LoadScenarioCommand");
+            return;
+        }
+
+        if (command.ArgCount == 0)
+        {
+            Logger.LogInformation("No arguments passed to LoadScenarioCommand");
+            return;
+        }
+
+        // Attempt to find the matching scenario
+        string scenarioName = command.ArgByIndex(0);
+        ScenarioConfig matchingScenario = _scenarioConfigs.Find(delegate (ScenarioConfig config)
+        {
+            return config.Name == scenarioName;
+        });
+
+        if (matchingScenario == null)
+        {
+            Server.PrintToConsole(String.Format("Could not find scenario: %s", scenarioName));
+            return;
+        }
+
+        // TODO: check that we're on the correct map
+        // TODO: check if there are sufficient players for configuration
+        // TODO: restart round, place players
+    }
 
     public override void Load(bool hotReload)
     {
-        // TODO: something interesting
+        // Just hard-code a scenario for now, we'll worry about persistence later
+        PlayerPlacement TTopMid;
+        TTopMid.Position = new Vector3() { x = 259.18f, y = 12.83f, z = -143.31f };
+        TTopMid.Angle = new Vector3() { x = -2.43f, y = -161.99f, z = 0f };
+        TTopMid.Team = CsTeam.Terrorist;
+        TTopMid.IsRequired = true;
+
+        PlayerPlacement CTWindow;
+        CTWindow.Position = new Vector3() { x = -1183.97f, y = -811.89f, z = -103.97f };
+        CTWindow.Angle = new Vector3() { x = 0f, y = 15.95f, z = 0f };
+        CTWindow.Team = CsTeam.CounterTerrorist;
+        CTWindow.IsRequired = true;
+
+        var currentConfig = new ScenarioConfig();
+        currentConfig.Name = "Mirage.1v1.WindowVsTopMid";
+        currentConfig.Map = Map.Mirage;
+        currentConfig.PlayerPlacements.Add(TTopMid);
+        currentConfig.PlayerPlacements.Add(CTWindow);
+
+        _scenarioConfigs.Add(currentConfig);
     }
 }
